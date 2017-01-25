d5f6cbe05010324007093882a1acf9df0d34807e
Daniel Sumba <sumbadaniel1@gmail.com>
2017-01-25 22:08:42 +0300

    contains addition timer and start function

--- pomodoro.py
+++ pomodoro.py
@@ -1,71 +1,79 @@
 from threading import Timer
 import time
-import datetime
+from datetime import datetime
+import sys
+import threading
+import time
+from os import system
+import pyglet
 
-class Pomodoro(object):
-	"""docstring for ClassName"""
-	def __init__(self, cycletime = 25, short_break = 5, long_break = 15):
-		super(Pomodoro, self).__init__()
-		self.time = time
-		self.short_break = short_break
-		self.long_break = long_break
-		self.run = True
+tasktime = 10
+breaktime = 2
+longtime = 5
+cycle = 0
 
-	"""def config_time(cycletime):
-		#gets the pomodoro cycle time and changes it
-		cycletime = input("Enter time in minutes taken to finish one pomodoro cycle: >")"""
 
-	def get_timer(self):
-		mins = 0
-		#Only run if user types "start"
+def longbreak():
+	global longtime
+	minutes = longtime
+	print ("LONG BREAK")
 
-		if self.run == True:
-			# Loop until we reach 25 minutes running
-			while mins != 1:
-				print (">>>>>>>>>>"), mins
-				#sleep for minute
-				time.sleep(60)
-				#Increment the minute total
-				mins += 1
-			# Brings up the dialog box here
+	while minutes:
+		mins, secs = divmod(minutes, 60)
+		timeformat = '{:02d}:{:02d}'.format(mins, secs)
+		print(timeformat, end='\r')
+		time.sleep(1)
+		minutes -=1
+	print("promodoro task done")
 
-	def stop_task(self):
-		tasks_done = {}
-		continue_cycle = input("continue task? ")
-		if continue_cycle == "yes":
-			Pomodoro.start_task(self)
-		else:
-			self.run = False
-		start_task()
+
+def shortbreak():
+	global breaktime
+	minutes = breaktime
+	#print ("SHORT BREAK")
+
+	while minutes:
+		mins, secs = divmod(minutes, 60)
+		timeformat = '{:02d}:{:02d}'.format(mins, secs)
+		print(timeformat, end='\r')
+		time.sleep(1)
+		minutes -= 1
+	timer()
+
+def timer():
+	global tasktime
+	global cycle
+	minutes = tasktime
+
+	while minutes:
+		mins, secs = divmod(minutes, 60)
+		timeformat = '{:02d}:{:02d}'.format(mins, secs)
+		print(timeformat, end='\r')
+		time.sleep(1)
+		minutes -=1
 	
-	def start_task(self):
-		task_title = input()
-		task_date = datetime.datetime.now() # takes date and time
+	cycle +=1 # cycle increment
 
-		#run = True
-		while True:
-			Pomodoro.get_timer(self)
-			if not stop_task(self):
-				break
+	if cycle != 4:
+		shortbreak()
+	else:
+		longbreak()
+		print("task done")
 
+def counter():
+	pass
 
+def start():
+	running = True
+	timer() #calls timer method
 
-	def config_short_break():
-		short_break = input("Enter the short break time in minutes: >")
-
-
-	def config_long_break():
-		long_break = input("Enter the long break time in minutes: >")
-
-
-	def config_sound():
-		pass
-
-	def list_tasks():
-		pass
-
-
-
-p = Pomodoro()
-p.start_task()
+def new_task():
+	# creates a new task, sets running to true and starts the timer method
+	cycle = 0
+	task_title = input("Enter Task title: ")
+	task_date = datetime.now();
+	tasks = task_title, task_dat
+	print(task_title)
+	start()
 		
+new_task()
